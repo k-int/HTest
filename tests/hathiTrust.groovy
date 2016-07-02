16aee7d6b5f39a4471799e4bbaf6b2a42f432bb7
Ian Ibbotson <ian.ibbotson@gmail.com>
2016-07-02 18:38:35 +0100

    Working for first page of oai records

--- tests/hathiTrust.groovy
+++ tests/hathiTrust.groovy
@@ -6,7 +6,11 @@
   // @GrabResolver(name='mvnRepository', root='http://central.maven.org/maven2/'),
   @Grab(group='org.apache.hbase', module='hbase-client', version='1.2.1'),
   @Grab(group='org.apache.hbase', module='hbase-common', version='1.2.1'),
-  @Grab(group='org.apache.hadoop', module='hadoop-common', version='2.7.2')
+  @Grab(group='org.apache.hadoop', module='hadoop-common', version='2.7.2'),
+  @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.2'),
+  @Grab(group='org.apache.httpcomponents', module='httpclient', version='4.5.2'),
+  @Grab(group='org.apache.httpcomponents', module='httpmime', version='4.5.2'),
+  @GrabExclude('org.codehaus.groovy:groovy-all')
 ])
 
 
@@ -19,10 +23,46 @@ import org.apache.hadoop.hbase.client.HTable
 import org.apache.hadoop.hbase.client.Put
 import org.apache.hadoop.hbase.util.Bytes
 import groovy.time.*
+import java.util.Properties
+import static groovy.json.JsonOutput.*
+import groovy.json.JsonSlurper
+import java.security.MessageDigest
+import groovyx.net.http.HTTPBuilder
+import org.apache.http.entity.mime.MultipartEntity
+import org.apache.http.entity.mime.HttpMultipartMode
+import org.apache.http.entity.mime.content.InputStreamBody
+import org.apache.http.entity.mime.content.StringBody
+import groovyx.net.http.*
+import org.apache.http.entity.mime.MultipartEntityBuilder /* we'll use the new builder strategy */
+import org.apache.http.entity.mime.content.ByteArrayBody /* this will encapsulate our file uploads */
+import org.apache.http.entity.mime.content.StringBody /* this will encapsulate string params */
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
+import groovyx.net.http.*
+
+config = null;
+cfg_file = new File('./sync-hathitrust-config.json')
+if ( cfg_file.exists() ) {
+  config = new JsonSlurper().parseText(cfg_file.text);
+}
+else {
+  config=[:]
+}
+
+println("Using config ${config}");
 
 println("NBK Ingest Phase 1 Adapter for hathitrust");
 
-addRecord([:]);
+println("Pulling latest messages");
+pullLatest(config);
+println("All done");
+
+println("Updating config");
+cfg_file.delete()
+cfg_file << toJson(config);
+
+
+// addRecord([:]);
 
 System.exit(0);
 
@@ -53,3 +93,120 @@ def addRecord(record) {
     e.printStackTrace()
   }
 }
+
+
+
+def pullLatest(config) {
+
+  // def cursor = SyncCursor.findByActivity('KBPlusTitles') ?: new SyncCursor(activity:'KBPlusTitles', lastTimestamp:0).save(flush:true, failOnError:true);
+  if ( config.cursor == null ) {
+    config.cursor = [ lastTimestamp : 0 ];
+  }
+
+  def cursor = config.cursor
+
+  // log.debug("Got cursor ${cursor}");
+
+  // http://quod.lib.umich.edu/cgi/o/oai/oai?verb=ListRecords&metadataPrefix=marc21&set=hathitrust
+  doSync('http://quod.lib.umich.edu','/cgi/o/oai/oai', 'marc21', 'hathitrust', cursor) { r ->
+
+    def result = [:]
+
+    try {
+      def sdf = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+      def record_ts_str = r.header.datestamp.text()
+      result.ts = sdf.parse(record_ts_str)?.getTime()
+
+      def identifiers = []
+      println("Process record ${r}");
+      // def title_id = r.metadata.kbplus.title.@id.text()
+    }
+    catch ( Exception e ) {
+      result.message = e.message;
+    }
+
+    result
+  }
+}
+
+public doSync(host, path, prefix, set, cursor, notificationTarget) {
+  println("Get latest changes ${host} ${path} ${prefix} ${cursor}");
+
+  def http = new HTTPBuilder( host )
+  http.ignoreSSLIssues()
+  http.contentType = XML
+  http.headers = [Accept : 'application/xml']
+  def lastTimestamp = 0;
+
+  def more = true
+  println("Attempt get...");
+
+  def resumption=null
+
+  // perform a GET request, expecting XML response data
+  while ( more ) {
+
+    println("Make request....");
+
+    def qry = null
+    if ( resumption ) {
+        println("Processing resumption");
+        qry = [ verb:'ListRecords', resumptionToken: resumption ]
+    }
+    else {
+        println("Fetch all records since ${(cursor?.lastTimestamp)?:0}");
+        def the_date = new Date(cursor.lastTimestamp)
+        def sdf = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        def from=sdf.format(the_date)
+        println("Requesting records since ${cursor.lastTimestamp} :: ${from}");
+        qry = [ verb:'ListRecords', metadataPrefix: prefix, from:from, set:set ]
+    }
+
+    println("Query params : ${qry} ");
+
+    http.request( GET, XML ) { req ->
+
+      uri.path = path
+      uri.query = qry 
+      contentType=XML
+
+      // response handler for a success response code:
+      response.success = { resp, xml ->
+        int ctr=0
+        println("In response handler");
+        println("Status ${resp.statusLine}")
+
+        def slurper = new groovy.util.XmlSlurper()
+        // def parsed_xml = slurper.parseText(xml.text)
+        def parsed_xml = slurper.parse(xml)
+
+        parsed_xml?.'ListRecords'?.'record'.each { r ->
+          def clr = notificationTarget(r)
+          println(clr);
+          ctr++
+          if ( clr.ts > lastTimestamp )
+            lastTimestamp = clr.ts
+        }
+
+        if ( ctr > 0 ) {
+          more=true
+        }
+        else {
+          more=false
+        }
+
+        resumption = parsed_xml?.'ListRecords'?.'resumptionToken'
+        println("Complete ${ctr} ${more} ${resumption}");
+      }
+
+      response.error = { err ->
+        println(err)
+      }
+    }
+    // update cursor
+    cursor.lastTimestamp = lastTimestamp
+    // cursor.save(flush:true, failOnError:true);
+  }
+}
+
